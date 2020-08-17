45938f6381f13cc6f4916e83f22efd6c22b6fbae
vickicres <66817698+vickicres@users.noreply.github.com>
2020-08-17 08:54:38 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -217,39 +217,36 @@ function filterNames(input) {
 
     //create error message when is no search result found ***** not working properly ***
     if (noFound) {
-        const errorMessage = document.createElement('h2');
-        errorMessage.className = 'no-result';
-        errorMessage.innerHTML = 'No Match Found';
-        gallery.appendChild(errorMessage);
+        let errorMessage = document.querySelector('.no-result');
+        if (!errorMessage) {
+            errorMessage = document.createElement('h2');
+            errorMessage.className = 'no-result';
+            errorMessage.innerHTML = 'No Match Found';
+            gallery.appendChild(errorMessage);
+        }
 
-        //      const showResults = document.querySelector('.no-result');
-        //      showResults.style.color = '#E25A53';
+    }
+    /***
+    ** ---------------------------------------
+       change background color and text color
+    ** ---------------------------------------
+    ***/
+
+    function changeBG() {
+        document.body.style.background = 'rgba(173, 220, 202)';
     }
 
-}
+    function changeText() {
+        document.querySelector('h1').style.color = 'darkblue';
 
-
-/***
-** ---------------------------------------
-   change background color and text color
-** ---------------------------------------
-***/
-
-function changeBG() {
-    document.body.style.background = 'rgba(173, 220, 202)';
-}
-
-function changeText() {
-    document.querySelector('h1').style.color = 'darkblue';
-
-}
-
-
-//click anywhere outside of the container to close modal
-window.addEventListener('click', (e) => {
-    const modalWindow = document.querySelector('.modal-container');
-
-    if (e.target == modalWindow) {
-        modalWindow.remove();
     }
-});
+
+
+    //click anywhere outside of the container to close modal
+    window.addEventListener('click', (e) => {
+        const modalWindow = document.querySelector('.modal-container');
+
+        if (e.target == modalWindow) {
+            modalWindow.remove();
+        }
+    });
